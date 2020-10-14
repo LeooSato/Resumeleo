0f8b303a09655abcde21c799727b8761f34f0d1e
leoosato <lksato63@gmail.com>
2020-10-14 12:03:02 -0300

    Updates

---
+++ precache-manifest.859a903d3632c123bd6092d00c8c3e7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b54596a07f6d4205cd37904b6d4b89f",
+    "url": "/resumeleo/index.html"
+  },
+  {
+    "revision": "4a0462ad44cd81bc8d4d",
+    "url": "/resumeleo/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "0d5a64567fdaa235fb5c",
+    "url": "/resumeleo/static/css/main.a8e75bf3.chunk.css"
+  },
+  {
+    "revision": "4a0462ad44cd81bc8d4d",
+    "url": "/resumeleo/static/js/2.533a62cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resumeleo/static/js/2.533a62cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5a64567fdaa235fb5c",
+    "url": "/resumeleo/static/js/main.ac1e7a13.chunk.js"
+  },
+  {
+    "revision": "322b971ca1aa59072236",
+    "url": "/resumeleo/static/js/runtime-main.48b529a5.js"
+  },
+  {
+    "revision": "6d64a5dafe150cb01fa3462824b45c0e",
+    "url": "/resumeleo/static/media/opai.6d64a5da.png"
+  }
+]);
